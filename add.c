16f78094cb26d1f39f9b6f9e5e1a29276ddd7671
Mark Hargreaves <clashclanacc2602@gmail.com>
2020-05-04 09:33:55 -0400

    WE DONT WANT THE BEST WAY

--- add.c
+++ add.c
@@ -10,18 +10,10 @@ extern int additionLEA(int, int);
 int main(){
     int i, rand1, rand2;
     struct timeval st, et;  
-    double time_spentBEST, time_spentADD, time_spentLEA;
+    double time_spentADD, time_spentLEA;
     srand(time(NULL));
     gettimeofday(&st, NULL);
-    for (i = 0; i < 1000; i++){
-        rand1 = rand()%256;
-        rand2 = rand()%256;
-        additionTHEBEST(rand1, rand2);
-    }
-    gettimeofday(&et, NULL);
-    time_spentBEST = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
-    gettimeofday(&st, NULL);
-    for (i = 0; i < 1000; i++){
+    for (i = 0; i < 1005000; i++){
         rand1 = rand()%256;
         rand2 = rand()%256;
         additionADD(rand1, rand2);
@@ -29,13 +21,14 @@ int main(){
     gettimeofday(&et, NULL);
     time_spentADD = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
     gettimeofday(&st, NULL);
-    for (i = 0; i < 1000; i++){
+    for (i = 0; i < 1005000; i++){
         rand1 = rand()%256;
         rand2 = rand()%256;
         additionLEA(rand1, rand2);
     }
     gettimeofday(&et, NULL);
     time_spentLEA = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
-    printf("THE BEST %f microsec, ADD %f microsec, LEA %f microsec\n", time_spentBEST, time_spentLEA, time_spentADD);
+    printf("ADD %f microsec, LEA %f microsec\n", time_spentLEA, time_spentADD);
+    printf("On your machine, ADD was faster by %f times\n", time_spentADD/time_spentLEA);
     return 0;
 }
