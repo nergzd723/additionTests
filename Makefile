863ec1f18e46d13686a6ae2351c94d54431e8044
Mark Hargreaves <clashclanacc2602@gmail.com>
2020-06-01 12:03:00 +0300

    buggy code

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 all:
 	nasm -felf64 add.asm
-	gcc -Wall -lgcc add.o add.c -o add
+	gcc -Wall -lgcc add.o add.c -o add -g
--- add.c
+++ add.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <x86intrin.h>
 
 extern int additionTHEBEST(int, int);
 extern int additionADD(int, int);
@@ -9,26 +10,41 @@ extern int additionLEA(int, int);
 
 int main(){
     int i, rand1, rand2;
-    struct timeval st, et;  
-    double time_spentADD, time_spentLEA;
+    long long sum;
+    unsigned long long a, b;
+    unsigned long long c;
+    __uint32_t AddArray[500];
+    __uint32_t LeaArray[500];
+    double avgADD, avgLEA;
     srand(time(NULL));
-    gettimeofday(&st, NULL);
-    for (i = 0; i < 1005000; i++){
+    for (i = 0; i < 50; i++){
         rand1 = rand()%256;
         rand2 = rand()%256;
+        a = _rdtsc();
         additionADD(rand1, rand2);
+        b = _rdtsc();
+        c = b-a;
+        AddArray[i] = (__uint32_t)c;
     }
-    gettimeofday(&et, NULL);
-    time_spentADD = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
-    gettimeofday(&st, NULL);
-    for (i = 0; i < 1005000; i++){
+    for (int loop = 0; loop < sizeof(AddArray); loop++){
+        sum = sum + AddArray[loop];
+    }
+    avgADD = (int)sum / sizeof(AddArray);
+    for (i = 0; i < 50; i++){
         rand1 = rand()%256;
         rand2 = rand()%256;
+        a = _rdtsc();
         additionLEA(rand1, rand2);
+        b = _rdtsc();
+        c = b-a;
+        LeaArray[i] = (__uint32_t)c;
     }
-    gettimeofday(&et, NULL);
-    time_spentLEA = ((et.tv_sec - st.tv_sec) * 1000000) + (et.tv_usec - st.tv_usec);
-    printf("ADD %f microsec, LEA %f microsec\n", time_spentLEA, time_spentADD);
-    printf("On your machine, ADD was faster by %f times\n", time_spentADD/time_spentLEA);
+    sum = 0;
+    for (int loop = 0; loop < sizeof(LeaArray); loop++){
+        sum = sum + LeaArray[loop];
+    }
+    avgLEA = (float)sum / sizeof(LeaArray);
+    printf("ADD %f microsec, LEA %f microsec\n", avgADD, avgLEA);
+    printf("On your machine, ADD was faster by %f times\n", avgADD/avgLEA);
     return 0;
 }
